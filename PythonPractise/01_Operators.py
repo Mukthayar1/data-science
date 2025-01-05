4d48bd186b12404ad8b9d8cbb90fd3bac815e0bb
Muhammad Mukthayar <64356624+Mukthayar1@users.noreply.github.com>
2025-01-05 18:37:03 +0500

    Initial commit

---
+++ PythonPractise/01_Operators.py
@@ -0,0 +1,10 @@
+print('''HELLO => codanics 
+      mukthayar
+      by the eas azwxw de sdsds 
+      ''')
+print("code commenting") #commented code <=>
+
+b = [1,3]
+c = [1,"3"]
+
+print( c == b )
