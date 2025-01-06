42b16c52571334784902e5c52831ba21ab700e1c
Muhammad Mukthayar <64356624+Mukthayar1@users.noreply.github.com>
2025-01-06 10:29:47 +0500

    day 5 done

---
+++ PythonPractise/03_OperatorsMore.py
@@ -0,0 +1,12 @@
+x = 15
+y = 4
+z = 5.5
+
+a = "Mukthayar"
+b = " mku"
+
+print(x+5,a)
+print(a+b)
+print(b > a)
+
+print(b > a and a > b)
---
+++ PythonPractise/O4_DataTypes.py
@@ -0,0 +1,23 @@
+# int
+x = 5 
+
+# float
+y = 5.5
+
+# string
+z = 'mukthayar'
+
+# list og int
+a = [1,2,3]
+
+# list of string
+b = ["b","c"]
+
+# c tuple
+c = (1,2,3,4,5)
+
+# dictionary
+d = {"saima":5,"mukthayar":5.7}
+
+# set
+d = {1,2,3,4}
