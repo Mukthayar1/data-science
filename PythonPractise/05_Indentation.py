c51378431698952a1a3143224f912b1b515492f0
Muhammad Mukthayar <khanadil1000s@gmail.com>
2025-01-06 12:26:39 +0500

    user inputs

---
+++ PythonPractise/05_Indentation.py
@@ -0,0 +1,13 @@
+a = 5 
+b = 15
+c= 16.8
+
+print(a < b)
+
+if a < b :
+        print("yes b is small")
+
+if c > b :
+        print("yes c is greater then b")
+else :
+        print("b is greater then c")
---
+++ PythonPractise/06_UserInputs.py
@@ -0,0 +1,22 @@
+# Age checking
+
+# personA = input("Please enter your name here : ")
+# personA_age = int(input(f"{personA}, please enter your age here: "))
+
+
+# personB = input("Please enter your partner name here : ")
+# personB_age = int(input(f"{personB}, please enter your age here: "))
+
+# if personA_age > personB_age :
+#     print(personA , " you are older then " , personB)
+# else :
+#      print(personB , " you are older then " , personA)
+
+# BMI Calculator
+
+Person_Weight = float(input("Please enter your weight in kgs : "))
+Person_Weight = float(input("Please enter your height in meters : "))
+
+bmi = (Person_Weight / (Person_Weight ** 2))
+
+print(f"Your BMI is = {bmi:.2f}")
