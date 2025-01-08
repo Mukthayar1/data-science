8eaa20bf48f5506e3409c8e1bd2deef9e435ffd3
Muhammad Mukthayar <khanadil1000s@gmail.com>
2025-01-08 12:40:30 +0500

    python errors

---
+++ PythonPractise/11_PythonErrors.py
@@ -0,0 +1,28 @@
+# syntax or indentation error
+x = 5
+
+if x > 3 :
+print("X IS POSITIVE")
+
+# logical or humain error
+if x > 3 :
+    print("Y IS NEGATIVE")
+
+#  runtime or module not found error
+print(np.sqrt(2))
+
+
+
+# syntax error
+# Name error
+# type error
+# value error
+# index error
+# key error
+# attribute error
+# zero division error
+# file not found error
+# import error
+# memory error
+# timeout error
+
